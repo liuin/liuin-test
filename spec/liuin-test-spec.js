732991acc7f77541f062bd2a80f50ed4e5d49eb6
liuin <20893791@qq.com>
2016-12-26 00:05:34 +0800

    1226

---
+++ spec/liuin-test-spec.js
@@ -0,0 +1,73 @@
+'use babel';
+
+import LiuinTest from '../lib/liuin-test';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('LiuinTest', () => {
+  let workspaceElement, activationPromise;
+
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace);
+    activationPromise = atom.packages.activatePackage('liuin-test');
+  });
+
+  describe('when the liuin-test:toggle event is triggered', () => {
+    it('hides and shows the modal panel', () => {
+      // Before the activation event the view is not on the DOM, and no panel
+      // has been created
+      expect(workspaceElement.querySelector('.liuin-test')).not.toExist();
+
+      // This is an activation event, triggering it will cause the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'liuin-test:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        expect(workspaceElement.querySelector('.liuin-test')).toExist();
+
+        let liuinTestElement = workspaceElement.querySelector('.liuin-test');
+        expect(liuinTestElement).toExist();
+
+        let liuinTestPanel = atom.workspace.panelForItem(liuinTestElement);
+        expect(liuinTestPanel.isVisible()).toBe(true);
+        atom.commands.dispatch(workspaceElement, 'liuin-test:toggle');
+        expect(liuinTestPanel.isVisible()).toBe(false);
+      });
+    });
+
+    it('hides and shows the view', () => {
+      // This test shows you an integration test testing at the view level.
+
+      // Attaching the workspaceElement to the DOM is required to allow the
+      // `toBeVisible()` matchers to work. Anything testing visibility or focus
+      // requires that the workspaceElement is on the DOM. Tests that attach the
+      // workspaceElement to the DOM are generally slower than those off DOM.
+      jasmine.attachToDOM(workspaceElement);
+
+      expect(workspaceElement.querySelector('.liuin-test')).not.toExist();
+
+      // This is an activation event, triggering it causes the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'liuin-test:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        // Now we can test for view visibility
+        let liuinTestElement = workspaceElement.querySelector('.liuin-test');
+        expect(liuinTestElement).toBeVisible();
+        atom.commands.dispatch(workspaceElement, 'liuin-test:toggle');
+        expect(liuinTestElement).not.toBeVisible();
+      });
+    });
+  });
+});
---
+++ spec/liuin-test-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LiuinTestView from '../lib/liuin-test-view';
+
+describe('LiuinTestView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
